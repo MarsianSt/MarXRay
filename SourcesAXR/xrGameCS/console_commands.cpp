dff38364ad25809c11f932a39bf8c0b4c9aa2e21
Dance Maniac <gazelev6@yandex.ru>
2026-02-03 17:34:37 +0300

    Уменьшен потолок команды hud_fov до 0.9
    На 1.0 появляются всякие неприятные баги

--- SourcesAXR/xrGameCS/console_commands.cpp
+++ SourcesAXR/xrGameCS/console_commands.cpp
@@ -2517,7 +2517,7 @@ void CCC_RegisterCommands()
 	CMD3(CCC_Mask,				"hud_crosshair",		&psHUD_Flags,	HUD_CROSSHAIR);
 	CMD3(CCC_Mask,				"hud_crosshair_dist",	&psHUD_Flags,	HUD_CROSSHAIR_DIST);
 
-	CMD4(CCC_Float,				"hud_fov",				&psHUD_FOV_def,	0.25f,	1.0f);
+	CMD4(CCC_Float,				"hud_fov",				&psHUD_FOV_def,	0.25f,	0.9f);
 	CMD4(CCC_Float,				"cam_fov",				&g_fov,			5.0f,	180.0f);
 	CMD3(CCC_Mask,				"ph_corpse_collision",	&psActorFlags,	AF_COLLISION);
 
