ec1e842839a8456fa315f46ee9d25ba524a3bd25
MarsianSt <MarsianSt@users.noreply.github.com>
2026-09-25 09:12:56 +1000

    refactor(xrEngine): remove disabled physics debug block

--- SourcesAXR/xrEngine/cf_dynamic_mesh.cpp
+++ SourcesAXR/xrEngine/cf_dynamic_mesh.cpp
@@ -44,14 +44,6 @@ BOOL	CCF_DynamicMesh::_RayQuery( const collide::ray_defs& Q, collide::rq_results
 			{
 				r.range = br.dist;
 			}
-#if	0
-			if(res)
-			{
-				ph_debug_render->open_cashed_draw();
-				ph_debug_render->draw_tri(  br.tri[0], br.tri[1], br.tri[2], D3DCOLOR_ARGB(50, 255,0,0), 0 );
-				ph_debug_render->close_cashed_draw(50000);
-			}
-#endif
 			return !res;
 		}
 		private:
