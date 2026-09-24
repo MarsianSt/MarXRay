22f6b3b6de04a6cca7550ff416e538c55bb5127a
MarsianSt <MarsianSt@users.noreply.github.com>
2026-09-25 09:03:30 +1000

    refactor(xrEngine): remove disabled caps lock code

--- SourcesAXR/xrEngine/line_edit_control.cpp
+++ SourcesAXR/xrEngine/line_edit_control.cpp
@@ -91,24 +91,7 @@ line_edit_control::~line_edit_control()
 
 static inline bool get_caps_lock_state	()
 {
-#if 0
-	static bool first_time					= true;
-	static bool is_windows_vista_or_later	= false;
-	if ( first_time ) {
-		first_time							= false;
-		OSVERSIONINFO						version_info;
-		ZeroMemory							( &version_info, sizeof(version_info) );
-		version_info.dwOSVersionInfoSize	= sizeof(version_info);
-		GetVersionEx						( &version_info );
-		is_windows_vista_or_later			= version_info.dwMajorVersion >= 6;
-	}
-
-	if ( is_windows_vista_or_later )
-		return		!!(GetKeyState(VK_CAPITAL) & 1);
-	else
-#else // #if 0
-		return		false;
-#endif // #if 0
+	return		false;
 }
 
 void line_edit_control::update_key_states	()
