a63d922c4e4e0cf509b014c71c4d0d877607503e
MarsianSt <MarsianSt@users.noreply.github.com>
2026-09-24 18:53:34 +1000

    style(headers): silence C4315 in shared headers without layout change

--- SourcesAXR/xrServerEntities/PropertiesListTypes.h
+++ SourcesAXR/xrServerEntities/PropertiesListTypes.h
@@ -68,7 +68,7 @@ public:
 	typedef fastdelegate::FastDelegate1<PropValue*> TOnChange;
     TOnChange			OnChangeEvent;
 public:
-						PropValue		():tag(0),m_Owner(0),OnChangeEvent(0){;}
+						PropValue		():tag(0),m_Owner(0){;}
 	virtual				~PropValue		(){}
     virtual xr_string	GetDrawText		(TOnDrawTextEvent OnDrawText)=0;
     virtual void		ResetValue		()=0;
@@ -155,7 +155,7 @@ public:
     };
     Flags32				m_Flags;
 public:
-						PropItem		(EPropType _type):type(_type),prop_color(0),val_color(0),item(0),key(0),OnClickEvent(0),OnDrawTextEvent(0),OnItemFocused(0){m_Flags.zero();}
+						PropItem		(EPropType _type):type(_type),prop_color(0),val_color(0),item(0),key(0){m_Flags.zero();}
 	virtual 			~PropItem		()
     {
     	for (PropValueIt it=values.begin(); values.end() != it; ++it)
@@ -374,8 +374,9 @@ public:
 public:
 	int					lim;
 public:
-						CTextValue		(LPSTR val, int _lim):value(val),init_value(val),lim(_lim)
+						CTextValue		(LPSTR val, int _lim):value(val),lim(_lim)
     {
+		init_value			= val;
     	OnBeforeEditEvent 	= 0;
         OnAfterEditEvent	= 0;
     };
